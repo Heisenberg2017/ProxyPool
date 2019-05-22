9f1b6ae3ff698588909f87fdf76821c365c27f9f
Heisenberg2017 <289957126@qq.com>
2019-05-22 20:37:08 +0800

    Python中的策略模式

--- proxypool/getter.py
+++ proxypool/getter.py
@@ -1,14 +1,14 @@
 from proxypool.log import logger
 from proxypool.db import RedisClient
-from proxypool.crawler import Crawler
+from proxypool.crawler import get_proxies
 from proxypool.settings import *
 import sys
+
 
 class Getter():
     def __init__(self):
         self.redis = RedisClient()
-        self.crawler = Crawler()
-    
+
     def is_over_threshold(self):
         """
         判断是否达到了代理池限制
@@ -21,10 +21,6 @@ class Getter():
     def run(self):
         logger.debug('获取器开始执行')
         if not self.is_over_threshold():
-            for callback_label in range(self.crawler.__CrawlFuncCount__):
-                callback = self.crawler.__CrawlFunc__[callback_label]
-                # 获取代理
-                proxies = self.crawler.get_proxies(callback)
-                sys.stdout.flush()
-                for proxy in proxies:
-                    self.redis.add(proxy)
+            # 获取代理
+            for proxy in get_proxies():
+                self.redis.add(proxy)
